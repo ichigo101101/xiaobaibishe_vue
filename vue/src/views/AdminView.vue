9be8661f35a58dc5c9b42d8b5dbe810a91d9bb6f
ichigo101101 <1429553410@qq.com>
2024-07-11 17:25:17 +0900

    p5 前后端链接上了5555

--- vue/src/views/AdminView.vue
+++ vue/src/views/AdminView.vue
@@ -7,9 +7,11 @@
         </div>
         <div>
             <el-table :data="tableData" style="width: 100%; margin: 15px 0px">
-                <el-table-column prop="date" label="日付" width="180"></el-table-column>
-                <el-table-column prop="name" label="名前" width="180"></el-table-column>
-                <el-table-column prop="address" label="アドレス"></el-table-column>
+<!--                <el-table-column prop="name" label="名前" width="180"></el-table-column>-->
+                <el-table-column prop="name" label="名前" ></el-table-column>
+                <el-table-column prop="sex" label="性別" ></el-table-column>
+                <el-table-column prop="age" label="年齢" ></el-table-column>
+                <el-table-column prop="phone" label="電話番号" ></el-table-column>
                 <el-table-column label="操作">
                     <el-button type="primary">編集</el-button>
                     <el-button type="danger">削除</el-button>
@@ -33,28 +35,53 @@
 </template>
 
 <script>
+    import request from "@/utils/request";
+
     export default {
+        name: "AdminView",
         data() {
             return {
-                tableData: [{
-                    date: '2016-05-02',
-                    name: '田中愛',
-                    address: '千葉県市川市平田４丁目'
-                }, {
-                    date: '2016-05-04',
-                    name: '田中愛',
-                    address: '千葉県市川市平田４丁目'
-                }, {
-                    date: '2016-05-01',
-                    name: '田中愛',
-                    address: '千葉県市川市平田４丁目'
-                }, {
-                    date: '2016-05-03',
-                    name: '田中愛',
-                    address: '千葉県市川市平田４丁目'
-                }]
+                tableData: []
             }
+        },
+        //页面加载的时候，做一些事情，在created里面
+        created() {
+            this.load();
+        },
+        //定义一些页面上控件触发的时间调用
+        methods: {
+            load() {
+                request.get("/admin").then(res => {
+                        if (res.code === '0' ) {
+                            this.tableData = res.data;
+                        }
+                    });
+
+            },
         }
     }
+    // export default {
+    //     data() {
+    //         return {
+    //             tableData: [{
+    //                 date: '2016-05-02',
+    //                 name: '田中愛',
+    //                 address: '千葉県市川市平田４丁目'
+    //             }, {
+    //                 date: '2016-05-04',
+    //                 name: '田中愛',
+    //                 address: '千葉県市川市平田４丁目'
+    //             }, {
+    //                 date: '2016-05-01',
+    //                 name: '田中愛',
+    //                 address: '千葉県市川市平田４丁目'
+    //             }, {
+    //                 date: '2016-05-03',
+    //                 name: '田中愛',
+    //                 address: '千葉県市川市平田４丁目'
+    //             }]
+    //         }
+    //     }
+    // }
 </script>
 
