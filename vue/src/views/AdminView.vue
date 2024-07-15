812fbb4d2125f0071e206622ebb25af03dafb8e6
ichigo101101 <1429553410@qq.com>
2024-07-15 18:18:18 +0900

    p5 增删改，有bug

--- vue/src/views/AdminView.vue
+++ vue/src/views/AdminView.vue
@@ -5,6 +5,7 @@
             <el-input v-model="params.phone" style="width: 200px; margin-left: 5px" placeholder="電話番号を入力してください"></el-input>
             <el-button type="warning" style="margin-left: 10px" @click="findBySearch()">捜査</el-button>
             <el-button type="warning" @click="reset()">リセット</el-button>
+            <el-button type="primary" style="margin-left: 10px" @click="add()">追加</el-button>
 <!--            <el-input style="width: 200px; margin-right: 10px" placeholder="内容を入力してください"></el-input>-->
 <!--            <el-button type="warning">捜査</el-button>-->
 <!--            <el-button type="primary">新規作成</el-button>-->
@@ -17,8 +18,14 @@
                 <el-table-column prop="age" label="年齢" ></el-table-column>
                 <el-table-column prop="phone" label="電話番号" ></el-table-column>
                 <el-table-column label="操作">
-                    <el-button type="primary">編集</el-button>
-                    <el-button type="danger">削除</el-button>
+                    <template slot-scope="scope">
+                        <el-button type="primary" @click="edit(scope.row)">編集</el-button>
+                        <!--                    <el-button type="danger">削除</el-button>-->
+                        <el-popconfirm title="确定删除吗？" @confirm="del(scope.row.id)">
+                            <el-button slot="reference" type="danger" style="margin-left: 5px">削除</el-button>
+                        </el-popconfirm>
+                    </template>
+
                 </el-table-column>
             </el-table>
 
@@ -35,17 +42,29 @@
                         :total="total">
                 </el-pagination>
             </div>
-<!--            <div class="block">-->
-<!--                <el-pagination-->
-<!--                        @size-change="handleSizeChange"-->
-<!--                        @current-change="handleCurrentChange"-->
-<!--                        :current-page="currentPage4"-->
-<!--                        :page-sizes="[10, 20, 30, 40]"-->
-<!--                        :page-size="10"-->
-<!--                        layout="total, sizes, prev, pager, next, jumper"-->
-<!--                        :total="400">-->
-<!--                </el-pagination>-->
-<!--            </div>-->
+            <div>
+                <el-dialog title="请填写信息" :visible.sync="dialogFormVisible" width="30%">
+                    <el-form :model="form">
+                        <el-form-item label="姓名" label-width="15%">
+                            <el-input v-model="form.name" autocomplete="off" style="width: 90%"></el-input>
+                        </el-form-item>
+                        <el-form-item label="性别" label-width="15%">
+                            <el-radio v-model="form.sex" label="男">男</el-radio>
+                            <el-radio v-model="form.sex" label="女">女</el-radio>
+                        </el-form-item>
+                        <el-form-item label="年龄" label-width="15%">
+                            <el-input v-model="form.age" autocomplete="off" style="width: 90%"></el-input>
+                        </el-form-item>
+                        <el-form-item label="电话" label-width="15%">
+                            <el-input v-model="form.phone" autocomplete="off" style="width: 90%"></el-input>
+                        </el-form-item>
+                    </el-form>
+                    <div slot="footer" class="dialog-footer">
+                        <el-button @click="dialogFormVisible = false">取 消</el-button>
+                        <el-button type="primary" @click="submit()">确 定</el-button>
+                    </div>
+                </el-dialog>
+            </div>
 
         </div>
 
@@ -67,7 +86,9 @@
                     pageSize: 5
                 },
                 tableData: [],
-                total: 0
+                total: 0,
+                dialogFormVisible: false,
+                form: {}
             }
         },
         //页面加载的时候，做一些事情，在created里面
@@ -114,6 +135,47 @@
                 this.params.pageNum = pageNum;
                 this.findBySearch();
             },
+            add() {
+                this.form = {};
+                this.dialogFormVisible = true;
+            },
+            edit(obj) {
+                this.form = obj;
+                this.dialogFormVisible = true;
+            },
+            submit() {
+                request.post("/admin", this.form).then(res => {
+                    if (res.code === '0') {
+                        this.$message({
+                            message: '操作成功',
+                            type: 'success'
+                        });
+                        this.dialogFormVisible = false;
+                        this.findBySearch();
+                    } else {
+                        this.$message({
+                            message: res.msg,
+                            type: 'success'
+                        });
+                    }
+                })
+            },
+            del(id) {
+                request.delete("/admin/" + id).then(res => {
+                    if (res.code === '0') {
+                        this.$message({
+                            message: '删除成功',
+                            type: 'success'
+                        });
+                        this.findBySearch();
+                    } else {
+                        this.$message({
+                            message: res.msg,
+                            type: 'success'
+                        });
+                    }
+                })
+            }
         },
 
     }
